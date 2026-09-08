b5186a30e8c071c466e717e49a9ff3c7224d4c97
Yashoda Shyaminda <yashodashaminda249@gmail.com>
2026-09-08 09:58:36 +0530

    Connect blockchain > sepolio testnet

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config(); // මේක අනිවාර්යයි! මේකෙන් තමයි .env ෆයිල් එක කියවන්නේ
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -13,16 +14,18 @@ module.exports = {
     },
   },
   networks: {
+    // 1. Local Network (Testing සඳහා)
     hardhat: {
-      chainId: 1337, // Standard local Hardhat chain ID
+      chainId: 1337, 
     },
     localhost: {
-      url: "http://127.0.0.1:8545",
+      url: "http://127.0.0.1:8545", // මේක ඔයාගේ ලැප්ටොප් එකේ ඇතුළේ දුවන Local URL එක නිසා කෝඩ් එකේ තිබ්බට කිසිම අවුලක් නෑ
       chainId: 1337,
     },
+    // 2. Testnet Deployment (Public)
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "https://rpc.ankr.com/eth_sepolia",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      url: process.env.SEPOLIA_RPC_URL || "", // URL එක එන්නේ කෙලින්ම .env එකෙන් 
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [], // Private Key එක එන්නේ කෙලින්ම .env එකෙන්
     },
   },
 };
